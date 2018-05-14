f2c542af2f61780b5cb4a1d3efcb1c4bd5968150
14049472 <14049472@myrp.edu.sg>
2018-05-14 14:46:17 +0800

    UPDATED

--- app/src/main/java/com/example/a14049472/demodatabase/MainActivity.java
+++ app/src/main/java/com/example/a14049472/demodatabase/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -14,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     Button btnInsert;
     Button btnGetTask;
     TextView tvResults;
+    ListView lv;
+    private ArrayAdapter aa;
+    ArrayList<Task> al;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         btnInsert = (Button) findViewById(R.id.btnInsert);
         btnGetTask=(Button)findViewById(R.id.btnGetTasks);
         tvResults=(TextView)findViewById(R.id.tvResults);
+        lv=(ListView)findViewById(R.id.lv);
+
+
 
         btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,11 +60,21 @@ public class MainActivity extends AppCompatActivity {
                 db.close();
 
                 String txt="";
+
                 for (int i=0; i<data.size();i++){
                     Log.d("Database Content",i+". "+data.get(i));
                     txt+=i+". " + data.get(i)+ "\n";
+
                 }
                 tvResults.setText(txt);
+
+                //CUSTOM LIST VIEW
+                DbHelper db2=new DbHelper(MainActivity.this);
+                al=db2.getTasks();
+                db2.close();
+
+                aa=new TaskAdapter(MainActivity.this,R.layout.row,al);
+                lv.setAdapter(aa);
             }
         });
 
